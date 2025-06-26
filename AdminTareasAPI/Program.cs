1c40b27ca3c97c494c26c46ef1f83947516740cf
Alfredoc <alfredo10396.c@gmail.com>
2025-06-25 21:59:59 -0600

    program.cs modified to allow localhost:4200

--- AdminTareasAPI/Program.cs
+++ AdminTareasAPI/Program.cs
@@ -15,6 +15,16 @@ builder.Services.AddScoped<ITareaRepository, TareaRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") 
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +33,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAngularApp");
+
 
 app.UseAuthorization();
 
